b7092cca9035e4f758df6d136413034db7b8bd46
kamatama41 <shiketaudonko41@gmail.com>
2019-03-29 15:55:43 +0900

    Add a main class to launch EmbulkServer

---
+++ src/main/java/org/embulk/executor/remoteserver/Launcher.java
@@ -0,0 +1,17 @@
+package org.embulk.executor.remoteserver;
+
+import java.io.IOException;
+
+public class Launcher {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 30001;
+        int numOfWorkers = 1;
+        if (args.length == 3) {
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+            numOfWorkers = Integer.parseInt(args[2]);
+        }
+        EmbulkServer.start(host, port, numOfWorkers);
+    }
+}
