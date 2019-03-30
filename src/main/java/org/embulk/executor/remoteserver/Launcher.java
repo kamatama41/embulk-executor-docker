af3b7cb223a84c8eae72d6b81e9211104730140d
kamatama41 <shiketaudonko41@gmail.com>
2019-03-30 14:57:12 +0900

    Make log level on EmbulkServer configurable

--- src/main/java/org/embulk/executor/remoteserver/Launcher.java
+++ src/main/java/org/embulk/executor/remoteserver/Launcher.java
@@ -1,5 +1,9 @@
 package org.embulk.executor.remoteserver;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class Launcher {
@@ -7,11 +11,19 @@ public class Launcher {
         String host = "0.0.0.0";
         int port = 30001;
         int numOfWorkers = 1;
-        if (args.length == 3) {
+        Level logLevel = Level.INFO;
+        if (args.length == 4) {
             host = args[0];
             port = Integer.parseInt(args[1]);
             numOfWorkers = Integer.parseInt(args[2]);
+            logLevel = Level.toLevel(args[3]);
         }
+        configureLogLevel(logLevel);
         EmbulkServer.start(host, port, numOfWorkers);
+    }
+
+    private static void configureLogLevel(Level logLevel) {
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        rootLogger.setLevel(logLevel);
     }
 }
